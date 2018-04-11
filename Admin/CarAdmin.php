2a5d728ae9ffed37ff931c743fc08001e47a0c30
DanBonix <danheavydrive@gmail.com>
2018-04-11 11:30:40 +0200

    Add files via upload

---
+++ Admin/CarAdmin.php
@@ -0,0 +1,191 @@
+<?php
+namespace AppBundle\Admin;
+use AppBundle\Entity\Car;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+class CarAdmin extends AbstractAdmin
+{
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('uuid')
+            ->add('registration')
+            ->add('firstRegistrationAt')
+            ->add('enabled')
+            ->add('status')
+            ->add('pickUpStatus')
+            ->add('pickUpDoneAt')
+            ->add('carType')
+            ->add('carEnergyType')
+            ->add('carMaker')
+            ->add('carCnit')
+            ->add('carModel')
+            ->add('carModelVariant')
+            ->add('carEngine')
+            ->add('firstHand')
+            ->add('duplicateKeys')
+            ->add('interiorColor')
+            ->add('exteriorColor')
+            ->add('passengerCapacity')
+            ->add('doors')
+            ->add('transmission')
+            ->add('mileageReal')
+            ->add('mileageEstimate')
+            ->add('lastMaintenanceMileage')
+            ->add('chassisJig')
+            ->add('repainted')
+            ->add('options', 'array')
+            ->add('pictures', 'array')
+            ->add('comments')
+        ;
+    }
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+       // dump($this->subject);
+        $formMapper
+            ->with('Pictures')
+                ->add('pictures', 'file', [
+                    'required' => false,
+                    'help' => 'b',
+                    "data_class" => null,
+                ])
+            ->end()
+            ->with('Général')
+                ->add('uuid', 'text')
+                ->add('carCnit', 'text', [
+                    'required' => false
+                ])
+                ->add('registration', 'text')
+                ->add('firstRegistrationAt', 'sonata_type_datetime_picker', [
+                    'data' => new \DateTime(), 'required' => false
+                ])
+                ->add('enabled', null, array('required' => false))
+            ->end()
+            ->with('Informations', array('collapsed' => true))
+                ->add('status', 'text')
+                ->add('pickUpStatus', 'text')
+                ->add('pickUpDoneAt', 'sonata_type_datetime_picker', [
+                    'data' => new \DateTime(), 'required' => false
+                ])
+            ->end()
+            ->with('Fiche technique', array('collapsed' => true))
+                ->add('carType', 'text', [
+                    'required' => false
+                ])
+                ->add('carEnergyType', 'text', [
+                    'required' => false
+                ])
+                ->add('carMaker', 'text', [
+                    'required' => false
+                ])
+                ->add('carModel', 'text', [
+                    'required' => false
+                ])
+                ->add('carModelVariant', 'text', [
+                    'required' => false
+                ])
+                ->add('carEngine', 'text', [
+                    'required' => false
+                ])
+                ->add('transmission', 'text', [
+                    'required' => false
+                ])
+                ->add('chassisJig', null, [
+                    'required' => false
+                ])
+            ->end()
+            ->with('Kilométrage', array('collapsed' => true))
+                ->add('mileageReal', 'text', [
+                    'required' => false
+                ])
+                ->add('mileageEstimate', 'text', [
+                    'required' => false
+                ])
+                ->add('lastMaintenanceMileage', 'text', [
+                    'required' => false
+                ])
+            ->end()
+            ->with('Détails du véhicule', array('collapsed' => true))
+                ->add('interiorColor', 'text', [
+                    'required' => false
+                ])
+                ->add('exteriorColor', 'text', [
+                    'required' => false
+                ])
+                ->add('passengerCapacity', 'text', [
+                    'required' => false
+                ])
+                ->add('doors', 'text', [
+                    'required' => false
+                ])
+                ->add('firstHand', null, [
+                    'required' => false
+                ])
+                ->add('duplicateKeys', null, [
+                    'required' => false
+                ])
+                ->add('repainted', null, [
+                    'required' => false
+                ])
+                ->add('options', 'text', [
+                    'required' => false
+                ])
+            ->end()
+            ->with('System Information', array('collapsed' => true))
+                ->add('createdAt', 'sonata_type_datetime_picker', [
+                    'data' => new \DateTime(), 'required' => false
+                ])
+                ->add('updatedAt', 'sonata_type_datetime_picker', [
+                    'data' => new \DateTime(), 'required' => false
+                ])
+            ->end()
+            ->with('Comments')
+                ->add('comments', 'textarea')
+            ->end();
+    }
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('uuid')
+            ->add('pictures','array', array('template' => 'backend/Media/list_image.html.twig'))
+            ->add('enabled')
+            ->add('registration')
+            ->add('firstRegistrationAt')
+            ->add('carType')
+            ->add('carEnergyType')
+            ->add('carModel')
+            ->add('interiorColor')
+            ->add('exteriorColor')
+            ->add('passengerCapacity')
+            ->add('doors')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ));
+    }
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('enabled');
+    }
+    public function prePersist($car) {
+        $this->saveFile($car);
+    }
+    public function preUpdate($car) {
+        $this->saveFile($car);
+    }
+    public function saveFile(Car $car) {
+        $basepath = $this->getRequest()->getBasePath();
+        $car->upload($basepath);
+    }
+}
---
+++ Admin/ClientAdmin.php
@@ -0,0 +1,112 @@
+<?php
+namespace AppBundle\Admin;
+use AppBundle\Entity\Agency;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+class ClientAdmin extends AbstractAdmin
+{
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('email')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('telephone')
+            ->add('lastLogin')
+            //->add('roles', 'collection')
+            /*->add('username')
+            ->add('usernameCanonical')
+            ->add('email')
+            ->add('emailCanonical')
+            ->add('enabled')
+            ->add('salt')
+            ->add('password')
+            ->add('plainPassword')
+            ->add('lastLogin')
+            ->add('confirmationToken')
+            ->add('passwordRequestedAt')
+            ->add('groups')
+            ->add('locked')
+            ->add('expired')
+            ->add('expiresAt')
+            ->add('credentialsExpired')
+            ->add('credentialsExpireAt')*/
+        ;
+    }
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $container = $this->getConfigurationPool()->getContainer();
+        $roles = $container->getParameter('security.role_hierarchy.roles');
+        dump($roles);
+        $rolesChoices = self::flattenRoles($roles);
+        dump($rolesChoices);
+        $formMapper
+            ->with('Général')
+            ->add('email', 'text', [
+                    'required' => false
+                ])
+                ->add('firstName', 'text')
+                ->add('lastName', 'text')
+                ->add('telephone', 'text')
+                ->add('lastLogin', 'sonata_type_datetime_picker', [
+                    'data' => new \DateTime(), 'required' => false
+                ])
+                //->add('enabled', null, array('required' => false))
+            ->end()
+            ->with('Security', array('collapsed' => true))
+                ->add('plainPassword', 'text', [
+                    'required' => false
+                ])
+                ->add('roles', 'choice', array(
+                        'choices'  => $rolesChoices,
+                        'multiple' => true,
+                        'expanded' => true
+                    )
+                )
+            ->end()
+        ;
+    }
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('email')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('telephone')
+            ->add('enabled')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show'   => array(),
+                    'edit'   => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+        ;
+    }
+    protected static function flattenRoles($rolesHierarchy)
+    {
+        $flatRoles = array();
+        foreach($rolesHierarchy as $roles) {
+            if(empty($roles)) {
+                continue;
+            }
+            foreach($roles as $role) {
+                if(!isset($flatRoles[$role])) {
+                    $flatRoles[$role] = $role;
+                }
+            }
+        }
+        return $flatRoles;
+    }
+}
